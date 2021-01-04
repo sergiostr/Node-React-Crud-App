77a8b30cf8622eed112e55895484b6e052a7f4a2
Web-developer <74660382+web-dev11@users.noreply.github.com>
2021-01-04 19:47:54 +0100

    complete

---
+++ backend/app/views/precache-manifest.cb62e067e08a02074e578965df950698.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87e0e5916a90150c8c3c304d6ecef592",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e8f245fa4d0c8b25769",
+    "url": "/static/css/2.3e575b9f.chunk.css"
+  },
+  {
+    "revision": "a17578e6f177e1c7c1d6",
+    "url": "/static/css/main.9c6cdb86.chunk.css"
+  },
+  {
+    "revision": "3e8f245fa4d0c8b25769",
+    "url": "/static/js/2.bbbe61ac.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/static/js/2.bbbe61ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a17578e6f177e1c7c1d6",
+    "url": "/static/js/main.8e725060.chunk.js"
+  },
+  {
+    "revision": "07858fea14fd66493418",
+    "url": "/static/js/runtime-main.efee8004.js"
+  }
+]);
